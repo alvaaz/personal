46ebbc0daaafe65d976c146c59b10fc574aca0a2
Queenie <queeniecmoya@gmail.com>
2019-07-30 09:20:03 -0400

    :bug: fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ import Image from './Image'
 const Nav = ({ siteTitle }) => (
 	<>
 		<div className="absolute right-0 top-0 w-64 h-64 bg-red rounded-full circle"></div>
-		<nav className="px-6 max-w-6xl mx-auto flex items-baseline justify-between flex-wrap py-6 mb-40 relative z-10">
+		<nav className="px-6 max-w-6xl mx-auto flex items-baseline justify-between flex-wrap py-6 mb-32 relative z-10">
 			<div className="flex items-center flex-shrink-0 text-white lg:flex-grow">
 				<Link to="/" className="text-3xl font-bold text-black">
 					{siteTitle}
--- src/pages/about.js
+++ src/pages/about.js
@@ -22,6 +22,59 @@ const About = () => (
 		<p className="pt-40 px-6 text-2xl font-regular leading-tight mb-10">
 			In every project I have been part of along my career, I have faced new areas. However, I have seen every challenge as an opportunity to learn and improve. I have also lead projects that have required good communication with the clients and an effective organization. In addition, I think that I have soft skills such as patience, honesty, and commitment, which are essential to finish any project successfully.
 		</p>
+
+
+		<div class="flex flex-wrap -mb-4">
+			<div class="w-1/3 mb-4">
+				<h1 class="text-5xl font-bold">Main skills.</h1>
+				<p class="text-3xl font-semibold">
+					User experience design (ux)
+				</p>
+				<p class="text-3xl font-semibold text-gray-500">
+					User experience design (ux)
+				</p>
+				<p class="text-3xl font-semibold">
+					User experience design (ux)
+				</p>
+				<p class="text-3xl font-semibold text-gray-500">
+					User experience design (ux)
+				</p>
+				<p class="text-3xl font-semibold">
+					User experience design (ux)
+				</p>
+				<p class="text-3xl font-semibold text-gray-500">
+					User experience design (ux)
+				</p>
+			</div>
+			<div class="w-1/3 mb-4">
+				<h1 class="text-5xl font-bold">Values.</h1>
+				<div className="pb-5 pt-4">
+					<p class="text-3xl font-semibold">
+						Good design is good business
+					</p>
+					<p class="text-3xl font-semibold text-gray-500">
+						User experience design (ux)
+					</p>
+				</div>
+				<div className="pb-5 pt-4">
+					<p class="text-3xl font-semibold">
+						Good design is good business
+					</p>
+					<p class="text-3xl font-semibold text-gray-500">
+						User experience design (ux)
+					</p>
+				</div>
+				<div className="pb-5 pt-4">
+					<p class="text-3xl font-semibold">
+						Good design is good business
+					</p>
+					<p class="text-3xl font-semibold text-gray-500">
+						User experience design (ux)
+					</p>
+				</div>
+
+			</div>
+		</div>
 	</Main>
   </>
 )
